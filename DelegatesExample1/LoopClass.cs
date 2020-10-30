1c278a5bd992cc1ab48f0c1511b71c03849182ec
hugoemanuelpintopaulo <hugoemanuelpintopaulo@gmail.com>
2020-10-30 13:07:42 +0200

    Add project files.

---
+++ DelegatesExample1/LoopClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DelegatesExample1
+{
+    //Signalton pattern
+    public class LoopClass
+    {
+        //This is a Delegate declaration.
+        //Difference between Task and Action, Action like void type has not return type. Task has a return type (ie return string, object, etc...)
+        private Action _LoopEndMessage;
+        private int _loopLength;
+
+        //Note optional parameters must be listed last
+        public LoopClass(Action LoopEndMessage, int loopLength = 0)
+        {
+            _loopLength = loopLength;
+
+            //This will hold the function we pass we initialise this class in the Program class in main
+            _LoopEndMessage = LoopEndMessage; 
+        }
+
+        public void RunLoop()
+        {
+            for (int i = 0; i < _loopLength; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Call the Delegqate function
+            _LoopEndMessage();
+        }        
+        
+    }
+}
---
+++ DelegatesExample1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DelegatesExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loopLength;
+
+            Console.WriteLine("Enter Length of Loop");
+            loopLength = Int32.Parse(Console.ReadLine());
+
+            LoopClass loop = new LoopClass(() => { Console.WriteLine("Loop Complete !!!"); }, loopLength);
+            loop.RunLoop();
+        }
+
+    }
+}
